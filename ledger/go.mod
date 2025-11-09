bd5db6a5bbca41b7e572985bd4b6f33cd189f52b
hh <hhio618@gmail.com>
2025-11-09 13:52:50 +0330

    Commit local changes before merging upstream

--- ledger/go.mod
+++ ledger/go.mod
@@ -4,6 +4,8 @@ go 1.22.0
 
 toolchain go1.22.10
 
+replace github.com/howeyc/ledger => github.com/hhio618/ledger v0.0.0-20241004213850-78a2602424d1
+
 require (
 	golang.org/x/mobile v0.0.0-20241204233305-ce44b2716d33 // indirect
 	golang.org/x/mod v0.22.0 // indirect
--- ledger/libledger.go
+++ ledger/libledger.go
@@ -1,8 +1,27 @@
 package libledger
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	ledger "github.com/howeyc/ledger"
+)
 
 // Exported function
 func Greet(name string) string {
-    return fmt.Sprintf("Hello, %s!", name)
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
+// Exported function
+func Balance(data []byte) string {
+	// b := bytes.NewBufferString(data)
+	reader := bytes.NewReader(data)
+	transactions, err := ledger.ParseLedger(reader)
+	bals := ledger.GetBalances(transactions, []string{})
+	if err != nil {
+		fmt.Errorf("Error: %v", err)
+	}
+	got, _ := json.Marshal(bals)
+	return string(got)
 }
